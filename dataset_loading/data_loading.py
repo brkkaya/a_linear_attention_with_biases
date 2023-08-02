77608c8a9affa15f918340c9943002655554a800
brkkaya <brk.kya34@yahoo.com>
2023-08-02 11:13:54 +0300

    re-implement dataloading and processing code

--- dataset_loading/data_loading.py
+++ dataset_loading/data_loading.py
@@ -9,43 +9,6 @@ from utils.utils import prepare_dataset
 PAD_TOKEN_ID = 0
 SEED = 42
 
-
-class CustomDataset(Dataset):
-    def __init__(self, data: dict, tokenizer, collate_fn: Callable = None):
-        # Process the data
-        if collate_fn is not None:
-            self.data = collate_fn(data, tokenizer)
-        else:
-            self.data = data
-        self.tokenizer = tokenizer
-
-    def __len__(self):
-        # Return the length of the dataset
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        # Get the text from the data
-        input_ids = self.data[idx]["input_ids"]
-        # input_ids_no_response = self.data[idx]["input_ids_no_response"]
-        labels = self.data[idx]["labels"]
-        return {
-            "input_ids": input_ids,
-            # "input_ids_no_response": input_ids_no_response,
-            "labels": labels,
-        }
-
-
-def create_dataloaders(data: pd.DataFrame, tokenizer, batch_size=8, shuffle=True, num_workers=4):
-    # Load the dataset
-
-    dataset = CustomDataset(data, tokenizer)
-    # Create the dataloaders
-    dataloader = DataLoader(dataset, batch_size, shuffle=False, num_workers=num_workers)
-
-    # Return the dataloaders
-    return dataloader
-
-
 def data_loading_custom_dataset(
     dataset_path: str,
     tokenizer_name: str,
@@ -89,20 +52,9 @@ def data_loading_hf_dataset(
     num_workers: int = 4,
 ):
     """Loaded dataset fields must have three columns or fields which are ['instruction','input','output']"""
-
     data = load_dataset(dataset_path)["train"]
     # Create the tokenizer
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
-    tokenizer.add_special_tokens({"pad_token": "<pad>"})
-    train_size = len(data) - test_size
-    data = pd.DataFrame(data)
-    data.rename({"context": "input", "response": "output"}, axis=1, inplace=True)  # this is used for given dataset.
-    data = data.to_dict("records")
-    train_set, test_set = random_split(
-        data,
-        lengths=(train_size, test_size),
-        generator=torch.Generator().manual_seed(SEED),
-    )
-    train_dataset = CustomDataset(train_set, tokenizer=tokenizer, collate_fn=prepare_dataset)
-    test_dataset = CustomDataset(test_set, tokenizer=tokenizer, collate_fn=prepare_dataset)
+
+    
     return train_dataset, test_dataset
--- utils/utils.py
+++ utils/utils.py
@@ -1,80 +1,24 @@
 import torch
 from transformers import AutoTokenizer
+from datasets import load_dataset
 from tqdm import tqdm
-
-IGNORE_INDEX = -1
-
-
-def prepare_dataset(data: list, tokenizer: AutoTokenizer, max_length: int = None):
-    if max_length is None:
-        max_length = tokenizer.model_max_length
-    return [
-        prepare_instance(sample, tokenizer=tokenizer, max_length=max_length, mask_inputs=False) for sample in tqdm(data)
-    ]
-
-
-def prepare_instance(example: dict, tokenizer: AutoTokenizer, max_length: int, mask_inputs: bool = True):
-    full_prompt = generate_prompt(example)
-    full_prompt_and_response = full_prompt + example["output"]
-    encoded_full_prompt, encoded_full_prompt_len = tokenize(
-        full_prompt, tokenizer=tokenizer, max_length=max_length, use_bos=True, use_eos=False
-    )
-    encoded_full_prompt_and_response, _ = tokenize(
-        full_prompt_and_response, tokenizer=tokenizer, use_bos=True, use_eos=True, max_length=max_length
-    )
-
-    # The labels are the full prompt with response, but with the prompt masked out
-    labels = encoded_full_prompt_and_response.clone()
-    if mask_inputs:
-        labels[:encoded_full_prompt_len] = IGNORE_INDEX
-
-    return {
-        **example,
-        "input_ids": encoded_full_prompt_and_response,
-        # "input_ids_no_response": encoded_full_prompt,
-        "labels": labels,
-    }
-
-
-def tokenize(
-    text: str,
-    tokenizer: AutoTokenizer,
-    max_length: int,
-    use_bos: bool = True,
-    use_eos: bool = True,
-    use_pad: bool = True,
-):
-    sample = tokenizer(
-        text,
-        max_length=max_length,
-        padding=False,
-        return_attention_mask=False,
-        return_special_tokens_mask=False,
-        truncation=True,
-        add_special_tokens=False,
-    )["input_ids"]
-
-    if use_eos:
-        sample = sample + [tokenizer.eos_token_id]
-    if use_bos:
-        sample = [tokenizer.bos_token_id] + sample
-
-    sample_length = len(sample)
-    padding_length = max_length - sample_length
-
-    if use_pad and max_length > sample_length:
-        sample += [tokenizer.pad_token_id] * padding_length
-        sample = torch.tensor(sample, dtype=torch.int)
-        return sample, sample_length
-    else:
-        sample = torch.tensor(sample, dtype=torch.int)
-        return sample, sample_length
 
 
 def generate_prompt(example):
     """Generates a standardized message to prompt the model with an instruction, optional input and a
-    'response' field."""
+    'output' field."""
 
     if example["input"]:
-        return f"### Instruction:\n{example['instruction']}\n\n### Input:\n{example['input']}\n\n### Response:"
-    return f"### Instruction:\n{example['instruction']}\n\n### Response:"
+        return f"### Instruction:\n{example['instruction']}\n\n### Input:\n{example['input']}\n\n### Response:\n{example['output']}\n"
+    return f"### Instruction:\n{example['instruction']}\n\n### Response:\n{example['output']}\n"
+
+
+def import_data(data_name_or_path:str):
+    if "json" in data_name_or_path:
+        data = load_dataset("json",data_files=data_name_or_path)
+    elif "csv" in data_name_or_path:
+        data = load_dataset("csv",data_files=data_name_or_path)
+    else:
+        data = load_dataset(data_name_or_path)
+    print()
+import_data("data/alpaca_cleaned.json")
